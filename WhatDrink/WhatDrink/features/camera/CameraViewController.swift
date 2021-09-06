644d66e0373fd783f1166dc11b4fe357c93ae38e
heegeepark <phg980513@naver.com>
2021-09-06 20:20:30 +0900

    Feat: 카메라뷰 스토리보드 설정 완료 - 뷰 및 버튼(뒤로가기, 스위치, 셔터) 추가

--- WhatDrink/WhatDrink/features/camera/CameraViewController.swift
+++ WhatDrink/WhatDrink/features/camera/CameraViewController.swift
@@ -8,9 +8,32 @@
 import UIKit
 
 class CameraViewController: UIViewController {
-    
+    @IBOutlet weak var previewView: PreviewView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var blurBGView: UIVisualEffectView!
+    @IBOutlet weak var captureButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func setupUI() {
+        captureButton.layer.cornerRadius = captureButton.bounds.height/2
+        captureButton.layer.masksToBounds = true
+        
+        blurBGView.layer.cornerRadius = blurBGView.bounds.height/2
+        blurBGView.layer.masksToBounds = true
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        
+    }
+    @IBAction func switchButtonTapped(_ sender: Any) {
+        
+    }
+    @IBAction func captureButtonTapped(_ sender: Any) {
+        
+    }
+    
 }
