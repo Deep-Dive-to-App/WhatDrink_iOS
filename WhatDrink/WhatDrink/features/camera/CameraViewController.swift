3ae5b0ee9bd4771b3588fb1faad3ec0b5f6c936e
heegeepark <phg980513@naver.com>
2021-09-09 13:42:48 +0900

    Feat: 카메라 전환 액션함수 구현 완료

--- WhatDrink/WhatDrink/features/camera/CameraViewController.swift
+++ WhatDrink/WhatDrink/features/camera/CameraViewController.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
 
 class CameraViewController: UIViewController {
+    let captureSession = AVCaptureSession()
+    var videoDeviceInput: AVCaptureDeviceInput!    // 전면, 후면 전환 가능하므로 var
+    var photoOutput: AVCapturePhotoOutput = {
+        let output = AVCapturePhotoOutput()
+        // 고해상도
+        output.isHighResolutionCaptureEnabled = true
+        return output
+    }()
+    
+    let sessionQueue = DispatchQueue(label: "session Queue")
+    let videoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInWideAngleCamera, .builtInTrueDepthCamera], mediaType: .video, position: .unspecified)
+    
     @IBOutlet weak var previewView: PreviewView!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var switchButton: UIButton!
     @IBOutlet weak var blurBGView: UIVisualEffectView!
     @IBOutlet weak var captureButton: UIButton!
     
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        previewView.session = captureSession
+        // 초기 설정
+        sessionQueue.async {
+            self.setupSession()
+            self.startSession()
+        }
+        setupUI()
     }
     
     func setupUI() {
@@ -26,14 +51,66 @@ class CameraViewController: UIViewController {
         blurBGView.layer.masksToBounds = true
     }
     
+    // 카메라 전환 액션함수
     @IBAction func backButtonTapped(_ sender: Any) {
-        
+        sessionQueue.async {
+            /// 반대편 카메라 찾기
+            let currentVideoDevice = self.videoDeviceInput.device
+            let currentPosition = currentVideoDevice.position
+            let isFront = currentPosition == .front
+            let preferredPosition: AVCaptureDevice.Position = isFront ? .back: .front
+            
+            /// 새로운 디바이스로 세션 update
+            let devices = self.videoDeviceDiscoverySession.devices
+            var newVideoDevice: AVCaptureDevice?
+            
+            newVideoDevice = devices.first(where: {device in
+                return preferredPosition == device.position
+            })
+            
+            if let newDevice = newVideoDevice {
+                do {
+                    let videoDeviceInput = try AVCaptureDeviceInput(device: newDevice)
+                    self.captureSession.beginConfiguration()
+                    self.captureSession.removeInput(self.videoDeviceInput)
+                    
+                    /// 새 device input 추가
+                    if self.captureSession.canAddInput(videoDeviceInput) {
+                        self.captureSession.addInput(videoDeviceInput)
+                        self.videoDeviceInput = videoDeviceInput
+                    } else {
+                        self.captureSession.addInput(self.videoDeviceInput)
+                    }
+                    self.captureSession.commitConfiguration()
+                } catch let error {
+                    print("error occured while creating device input: \(error.localizedDescription)")
+                }
+            }
+        }
     }
     @IBAction func switchButtonTapped(_ sender: Any) {
         
     }
     @IBAction func captureButtonTapped(_ sender: Any) {
+        let videoPreviewLayerOrientation = self.previewView.videoPreviewLayer.connection?.videoOrientation
+        sessionQueue.async {
+            let connection = self.photoOutput.connection(with: .video)
+        }
+    }
+    
+}
+
+// MARK: - Setup session & preview
+extension CameraViewController {
+    func setupSession() {
         
     }
     
+    func startSession() {
+        
+    }
+    
+    func stopSession() {
+        
+    }
 }
